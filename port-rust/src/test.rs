dcac0c6b3fffda44ee273e762a89f866d70422f6
Atharv <atharvrawal04@gmail.com>
2025-07-17 10:12:26 +0530

    feat: moved to serde_yml

--- port-rust/src/test.rs
+++ port-rust/src/test.rs
@@ -1,8 +1,8 @@
-use serde_yaml::{Value};
+use serde_yml::{Value};
 use std::fs;
 
 fn main() {
-    let data: Value = serde_yaml::from_str(&fs::read_to_string("config.yaml").unwrap()).unwrap();
+    let data: Value = serde_yml::from_str(&fs::read_to_string("config.yaml").unwrap()).unwrap();
     println!("{:#?}", data["http"]);
 if let Some(http) = data["http"].as_mapping() {
         for (key, value) in http {
