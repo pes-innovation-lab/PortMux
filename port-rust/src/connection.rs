dc822180bb2eb16717254afc01fbf8e7f6c084c9
Darshana Muraly <dm920059@gmail.com>
2025-07-25 13:14:17 +0530

    fix: more magic number fix

--- port-rust/src/connection.rs
+++ port-rust/src/connection.rs
@@ -2,11 +2,15 @@ use crate::protocol::Protocol;
 use tokio::io::{AsyncWriteExt, copy_bidirectional_with_sizes};
 use tokio::net::TcpStream;
 
+static LATENCY_BUFFER_SIZE:u32 = 1024;
+static THROUGHPUT_BUFFER_SIZE:u32 = 32768;
+static DEFAULT_BUFFER_SIZE:u32 = 8192;
+
 pub async fn handle_connection(mut client_socket: TcpStream, protocol: Protocol, buffer: Vec<u8>) {
     let (a_b_buffer, b_a_buffer) = match protocol.priority.as_str() {
-        "latency" => (1024, 1024),
-        "throughput" => (32768, 32768),
-        _ => (8192, 8192),
+        "latency" => (LATENCY_BUFFER_SIZE, LATENCY_BUFFER_SIZE),
+        "throughput" => (THROUGHPUT_BUFFER_SIZE, THROUGHPUT_BUFFER_SIZE),
+        _ => (DEFAULT_BUFFER_SIZE, DEFAULT_BUFFER_SIZE),
     };
 
     match TcpStream::connect(format!("127.0.0.1:{}", protocol.port)).await {
@@ -16,7 +20,7 @@ pub async fn handle_connection(mut client_socket: TcpStream, protocol: Protocol,
                 return;
             }
 
-            match copy_bidirectional_with_sizes(&mut client_socket, &mut service_socket, a_b_buffer, b_a_buffer).await {
+            match copy_bidirectional_with_sizes(&mut client_socket, &mut service_socket, a_b_buffer as usize, b_a_buffer as usize).await {
                 Ok(_) => println!("Client disconnected."),
                 Err(err) => eprintln!("Data copy error: {}", err),
             }
